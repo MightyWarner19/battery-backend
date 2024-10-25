6871f4e9f2f39aa8e4c47b23d63be86adf4328f0
ianupam001 <anupamtiwari8090@gmail.com>
2024-10-25 16:03:14 +0530

    fixes

--- api/controllers/testimonial.controller.js
+++ api/controllers/testimonial.controller.js
@@ -1,18 +1,20 @@
-import Tesimonial from '../models/testimonial.model.js';
-import { errorHandler } from '../utils/error.js';
+import Tesimonial from "../models/testimonial.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const create = async (req, res, next) => {
   if (!req.user.isAdmin) {
-    return next(errorHandler(403, 'You are not allowed to create a testimonial'));
+    return next(
+      errorHandler(403, "You are not allowed to create a testimonial")
+    );
   }
-  if (!req.body.title || !req.body.count) {
-    return next(errorHandler(400, 'Please provide all required fields'));
+  if (!req.body.content || !req.body.title) {
+    return next(errorHandler(400, "Please provide all required fields"));
   }
   const slug = req.body.title
-    .split(' ')
-    .join('-')
+    .split(" ")
+    .join("-")
     .toLowerCase()
-    .replace(/[^a-zA-Z0-9-]/g, '');
+    .replace(/[^a-zA-Z0-9-]/g, "");
   const newTesimonial = new Tesimonial({
     ...req.body,
     slug,
@@ -30,7 +32,7 @@ export const gettestimonials = async (req, res, next) => {
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
-    const sortDirection = req.query.order === 'asc' ? 1 : -1;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
     const testimonials = await Tesimonial.find({
       ...(req.query.userId && { userId: req.query.userId }),
       ...(req.query.category && { category: req.query.category }),
@@ -38,8 +40,8 @@ export const gettestimonials = async (req, res, next) => {
       ...(req.query.testimonialId && { _id: req.query.testimonialId }),
       ...(req.query.searchTerm && {
         $or: [
-          { title: { $regex: req.query.searchTerm, $options: 'i' } },
-          { content: { $regex: req.query.searchTerm, $options: 'i' } },
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
         ],
       }),
     })
@@ -62,7 +64,7 @@ export const gettestimonials = async (req, res, next) => {
     });
 
     res.status(200).json({
-        testimonials,
+      testimonials,
       totalTesimonials,
       lastMonthTesimonials,
     });
@@ -73,11 +75,13 @@ export const gettestimonials = async (req, res, next) => {
 
 export const deletetestimonial = async (req, res, next) => {
   if (!req.user.isAdmin || req.user.id !== req.params.userId) {
-    return next(errorHandler(403, 'You are not allowed to delete this testimonial'));
+    return next(
+      errorHandler(403, "You are not allowed to delete this testimonial")
+    );
   }
   try {
     await Tesimonial.findByIdAndDelete(req.params.testimonialId);
-    res.status(200).json('The testimonial has been deleted');
+    res.status(200).json("The testimonial has been deleted");
   } catch (error) {
     next(error);
   }
@@ -85,7 +89,9 @@ export const deletetestimonial = async (req, res, next) => {
 
 export const updatetestimonial = async (req, res, next) => {
   if (!req.user.isAdmin || req.user.id !== req.params.userId) {
-    return next(errorHandler(403, 'You are not allowed to update this testimonial'));
+    return next(
+      errorHandler(403, "You are not allowed to update this testimonial")
+    );
   }
   try {
     const updatedTesimonial = await Tesimonial.findByIdAndUpdate(
