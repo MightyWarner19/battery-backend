e2fe5be9acd856003502e8dc5606505f82c96fcc
ianupam001 <anupamtiwari8090@gmail.com>
2024-10-29 17:56:54 +0530

    fixes

--- api/index.js
+++ api/index.js
@@ -38,8 +38,8 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
+app.listen(3001, () => {
+  console.log("Server is running on port 3001!");
 });
 
 app.use("/api/user", userRoutes);
